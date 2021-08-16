d8f48e69e040877e65eb9f51e35bfb89548eea9d
Danuarta Wiratama <danuarta.wiratama@gdn-commerce.com>
2021-08-16 23:26:15 +0700

    feat: Scoreboard

--- src/views/Scoreboard.vue
+++ src/views/Scoreboard.vue
@@ -35,99 +35,19 @@ export default {
     search: "",
     headers: [
       {
-        text: 'ID Anggaran',
+        text: 'Nama Kandidat',
         align: 'center',
-        value: 'id_anggaran',
+        value: 'candidateName',
       },
       {
-        text: 'ID OPD',
+        text: 'Jumlah Suara',
         align: 'center',
-        value: 'id_opd',
+        value: 'candidateCount',
       },
       {
-        text: 'Jenis Instansi',
+        text: 'Persentase (%)',
         align: 'center',
-        value: 'jns_instansi',
-      },
-      {
-        text: 'KD Urusan',
-        align: 'center',
-        value: 'kd_urusan',
-      },
-      {
-        text: 'KD Bidang Urusan',
-        align: 'center',
-        value: 'kd_bidurusan',
-      },
-      {
-        text: 'KD Program',
-        align: 'center',
-        value: 'kd_program',
-      },
-      {
-        text: 'KD Kegiatan',
-        align: 'center',
-        value: 'kd_kegiatan',
-      },
-      {
-        text: 'KD Sub Kegiatan',
-        align: 'center',
-        value: 'kd_subkegiatan',
-      },
-      {
-        text: 'ID Fungsi',
-        align: 'center',
-        value: 'id_fungsi',
-      },
-      {
-        text: 'Uraian',
-        align: 'center',
-        value: 'uraian',
-      },
-      {
-        text: 'HD Anggaran',
-        align: 'center',
-        value: 'hd_anggaran',
-      },
-      {
-        text: 'Clevel',
-        align: 'center',
-        value: 'clevel',
-      },
-      {
-        text: 'HD',
-        align: 'center',
-        value: 'hd',
-      },
-      {
-        text: 'ID Rencana',
-        align: 'center',
-        value: 'id_rencana',
-      },
-      {
-        text: 'Nilai',
-        align: 'center',
-        value: 'nilai',
-      },
-      {
-        text: 'Nilai Ubah',
-        align: 'center',
-        value: 'nilai_ubah',
-      },
-      {
-        text: 'Realisasi',
-        align: 'center',
-        value: 'realisasi',
-      },
-      {
-        text: 'TU Capai',
-        align: 'center',
-        value: 'tu_capai',
-      },
-      {
-        text: 'TK Capai',
-        align: 'center',
-        value: 'tk_capai',
+        value: 'percentage',
       },
     ],
     items: [],
@@ -137,9 +57,9 @@ export default {
   },
   methods: {
     getAllAffectedApp: function () {
-      this.$http.get(process.env.VUE_APP_ALICE) //NANTI GANTI SESUAI DOMAIN
+      this.$http.get(process.env.VUE_APP_VOTE+"cast-vote") //NANTI GANTI SESUAI DOMAIN
           .then(response => {
-            this.items = response.data.content
+            this.items = response.data.candidateStandings
           })
           .catch(e => {
             console.log(e)
